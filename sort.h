92d8e6d21adbec97668b051e0b74a4a67ca92338
2012Inga <inganathi2001@gmail.com>
2024-01-22 09:40:01 -0800

    quick_sort

--- sort.h
+++ sort.h
@@ -2,6 +2,8 @@
 #define SORT_H
 
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -18,6 +20,9 @@ typedef struct listint_s
 
 void print_array(const int *array, size_t size);
 void selection_sort(int *array, size_t size);
+
+void print_array(const int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
