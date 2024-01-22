67de6ad8abe01323a84e57a2097d9bcccceb6655
2012Inga <inganathi2001@gmail.com>
2024-01-22 09:59:11 -0800

    shell_sort task done!

--- sort.h
+++ sort.h
@@ -11,6 +11,8 @@
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
+
+/* Doubly linked list structure */
 typedef struct listint_s
 {
 	int n;
@@ -18,6 +20,7 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/* Function prototypes */
 void print_array(const int *array, size_t size);
 void selection_sort(int *array, size_t size);
 
@@ -29,5 +32,11 @@ void insertion_sort_list(listint_t **list);
 
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
+
+void shell_sort(int *array, size_t size);
+
+/* Function prototypes for list manipulation */
+void printlist(const listint_t *list);
+void insertion_sort_list(listint_t **list);
 
 #endif /* SORT_H */
